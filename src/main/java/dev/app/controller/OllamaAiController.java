2cd272d71af20e7a12bc2b876761c1225e854db9
Prashant33-py <pvasala786@gmail.com>
2025-07-24 20:13:27 +0530

    Adds prompt template for movie recommendation

--- src/main/java/dev/app/controller/OllamaAiController.java
+++ src/main/java/dev/app/controller/OllamaAiController.java
@@ -1,12 +1,11 @@
 package dev.app.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.retry.NonTransientAiException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -32,6 +31,24 @@ public class OllamaAiController {
         } catch (NonTransientAiException e){
             return e.getMessage().split(" - ")[1];
          }
+    }
+
+    @PostMapping("/recommend")
+    public String getMovieRecommendation(@RequestParam String genre, @RequestParam String year, @RequestParam String language) {
+
+        String template = """
+                I want to watch {genre} movies around the year {year} in {language} language.
+                Can you recommend some movies for me? Suggest me only 1 movie with the length of the movie.
+                The response should be in the following json format and it should contain title, length, cast, director and imdbRating.
+                The cast should be a list of strings and the imdbRating should be a number.
+                """;
+        PromptTemplate promptTemplate = new PromptTemplate(template);
+        Prompt prompt = promptTemplate.create(Map.of("genre", genre, "year", year, "language", language));
+
+        return chatClient
+                .prompt(prompt)
+                .call()
+                .content();
     }
 
 }
